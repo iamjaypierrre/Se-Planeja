34154e0a1d1f835c443b59ced91d1c46053a6b51
iamjaypierre <jepiierre@gmail.com>
2022-09-06 16:09:24 -0300

    Initial commit/AppFinalizado

---
+++ app/src/main/java/com/example/seplaneja/activity/CadastroActivity.java
@@ -0,0 +1,122 @@
+package com.example.seplaneja.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.seplaneja.R;
+import com.example.seplaneja.config.ConfiguracaoFirebase;
+import com.example.seplaneja.helper.Base64Custom;
+import com.example.seplaneja.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private EditText campoNome, campoEmail, campoSenha;
+    private Button botaoCadastrar;
+    private FirebaseAuth autenticacao;
+    private Usuario usuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+
+        campoNome = findViewById(R.id.editNome);
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+        botaoCadastrar = findViewById(R.id.buttonCadastrar);
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textoNome = campoNome.getText().toString();
+                String textoEmail = campoEmail.getText().toString();
+                String textoSenha = campoSenha.getText().toString();
+
+                //Validar se os campos foram preenchidos
+                if (!textoNome.isEmpty()){
+                    if (!textoEmail.isEmpty()){
+                        if (!textoSenha.isEmpty()){
+
+                            usuario = new Usuario();
+                            usuario.setNome(textoNome);
+                            usuario.setEmail(textoEmail);
+                            usuario.setSenha(textoSenha);
+                            cadastrarUsuario();
+
+                            cadastrarUsuario();
+
+                        }else{
+                            Toast.makeText(CadastroActivity.this,
+                                    "Digite uma senha!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }else {
+                        Toast.makeText(CadastroActivity.this,
+                                "Preencha seu email!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else {
+                    Toast.makeText(CadastroActivity.this,
+                            "Preencha o nome!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    public void cadastrarUsuario(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+
+                String idUsuario = Base64Custom.codificarBase64(usuario.getEmail());
+                usuario.setIdUsuario(idUsuario);
+                usuario.salvar();
+                finish();
+
+            }else {
+
+                String excecao = "";
+                try {
+                    throw task.getException();
+                }catch(FirebaseAuthWeakPasswordException e){
+                    excecao = "Digite uma senha mais forte!";
+                } catch(FirebaseAuthInvalidCredentialsException e) {
+                    excecao = "Por favor, Digite um e-mail valido!";
+                }catch (FirebaseAuthUserCollisionException e){
+                    excecao = "Esta conta ja foi cadastrada";
+                }catch (Exception e){
+                    excecao = "Erro ao cadastrar usuario: " + e.getMessage();
+                    e.printStackTrace();
+                }
+
+                Toast.makeText(CadastroActivity.this,
+                        excecao,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/seplaneja/activity/DespesasActivity.java
@@ -0,0 +1,143 @@
+package com.example.seplaneja.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.seplaneja.R;
+import com.example.seplaneja.config.ConfiguracaoFirebase;
+import com.example.seplaneja.helper.Base64Custom;
+import com.example.seplaneja.helper.DateCustom;
+import com.example.seplaneja.model.Movimentacao;
+import com.example.seplaneja.model.Usuario;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+public class DespesasActivity extends AppCompatActivity {
+
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
+    private Movimentacao movimentacao;
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private Double despesaTotal;
+    //private Double despesaGerada;
+    //private Double despesaAtualizada;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_despesas);
+
+        campoValor = findViewById(R.id.editValor);
+        campoData = findViewById(R.id.editData);
+        campoCategoria = findViewById(R.id.editCategoria);
+        campoDescricao = findViewById(R.id.editDescricao);
+
+        //Preenche o campo data com a date atual
+        campoData.setText(DateCustom.dataAtual());
+        recuperarDespesaTotal();
+
+    }
+
+    public void salvarDespesa(View view) {
+
+        if (validarCamposDespesa()) {
+
+            movimentacao = new Movimentacao();
+            String data = campoData.getText().toString();
+            Double valorRecuperado = Double.parseDouble(campoValor.getText().toString());
+            movimentacao.setValor(valorRecuperado);
+            movimentacao.setCategoria(campoCategoria.getText().toString());
+            movimentacao.setDescricao(campoDescricao.getText().toString());
+            movimentacao.setData(data);
+            movimentacao.setTipo("d");
+
+            Double despesaAtualizada = despesaTotal + valorRecuperado;
+            atualizarDespesa(despesaAtualizada);
+
+            movimentacao.salvar(data);
+
+            finish();
+
+        }
+    }
+
+    public Boolean validarCamposDespesa() {
+
+        String textoValor = campoValor.getText().toString();
+        String textoData = campoData.getText().toString();
+        String textoCategoria = campoCategoria.getText().toString();
+        String textoDescricao = campoDescricao.getText().toString();
+
+        if (!textoValor.isEmpty()) {
+            if (!textoData.isEmpty()) {
+                if (!textoCategoria.isEmpty()) {
+                    if (!textoDescricao.isEmpty()) {
+                        return true;
+                    } else {
+                        Toast.makeText(DespesasActivity.this,
+                                "Descricao nao foi preenchida!",
+                                Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                } else {
+                    Toast.makeText(DespesasActivity.this,
+                            "Categoria nao foi preenchida!",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            } else {
+                Toast.makeText(DespesasActivity.this,
+                        "Data nao foi preenchida!",
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else {
+            Toast.makeText(DespesasActivity.this,
+                    "Valor nao foi preenchido!",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    public void recuperarDespesaTotal(){
+
+       String emailUsuario = autenticacao.getCurrentUser().getEmail();
+       String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+                despesaTotal = usuario.getDespesaTotal();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    public void atualizarDespesa(Double despesa){
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef
+                        .child("usuarios")
+                        .child(idUsuario);
+
+        usuarioRef.child("despesaTotal").setValue(despesa);
+    }
+
+}
